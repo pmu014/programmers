cf8e31eea17a2269fc30bef1af22bb7b74bf0b47
Minuk <108844082+pmu014@users.noreply.github.com>
2024-01-15 04:10:17 +0900

    Create strListChange.java

---
+++ JAVA/strListChange.java
@@ -0,0 +1,9 @@
+class strListChange {
+    public String solution(String[] arr) {
+        String answer = "";
+        for(int i=0; i<arr.length; i++){
+            answer += arr[i];
+        };
+        return answer;
+    }
+}
