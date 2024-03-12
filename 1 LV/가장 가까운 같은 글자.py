f965a436df21cae0fb8f23feff2e5c639bc54885
Minuk <108844082+pmu014@users.noreply.github.com>
2024-03-13 03:13:06 +0900

    Create 가장 가까운 같은 글자.py

---
+++ 1 LV/가장 가까운 같은 글자.py
@@ -0,0 +1,10 @@
+def solution(s):
+    dic = {}
+    answer = []
+    for i in range(len(s)):
+        if s[i] in dic:
+            answer.append(i - dic[s[i]])
+        else:
+            answer.append(-1)
+        dic[s[i]] = i    
+    return answer 
